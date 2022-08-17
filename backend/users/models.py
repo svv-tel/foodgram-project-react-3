41ec1372cbd7196fa6be141a444157b385c810a5
Timofey <vorobiev.te@yandex.ru>
2022-08-17 18:30:35 +0300

    Ñfollow_model

--- backend/users/models.py
+++ backend/users/models.py
@@ -36,3 +36,30 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Follow(models.Model):
+    """Модель подписки"""
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        related_name='follower',
+        on_delete=models.CASCADE
+    )
+
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        related_name='following',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'author',),
+                name='unique_subscribe'
+            ),
+        )
