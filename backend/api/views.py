782ab6f9e6b8b8168b039c38ec4d4ffc893f5d50
Timofey <vorobiev.te@yandex.ru>
2022-08-20 15:19:29 +0300

    tagsview

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,31 @@
 from djoser.views import UserViewSet
-from rest_framework.generics import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from recipes.models import Recipe, Tag, Ingredient, Favorite
+from rest_framework.permissions import AllowAny
 from users.models import User
 from .serializers import (RecipeSerializer, IngredientSerializer,
                           CustomUserSerializer,
                           TagSerializer, FavoriteSerializer)
 
 
-class CustomUserViewSet(viewsets.ModelViewSet):
+class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
 
 
-class TagsViewSet(viewsets.ModelViewSet):
+class RetrieveListViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    pass
+
+
+class TagsViewSet(RetrieveListViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    permission_classes = (AllowAny,)
+    pagination_class = None
 
 
 class IngredientsViewSet(viewsets.ModelViewSet):
