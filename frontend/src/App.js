1f410d4793ee721f820d1bb5b63043fbed434b5c
Timofey <vorobiev.te@yandex.ru>
2022-08-28 13:23:20 +0300

    2nd-review_deploy_50-6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -263,8 +263,8 @@ function App() {
               onSignUp={registration}
             />
           </Route>
-          <Route path='/'>
-            {loggedIn ? <Redirect to='/recipes' /> : <Redirect to='/recipes'/>}
+          <Route exact path='/'>
+            <Redirect to='/recipes'/>
           </Route>
         </Switch>
         <Footer />
